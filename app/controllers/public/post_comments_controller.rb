85fda5dedd48f2fd4d9695d5f3d67bc6567c2321
EC2 Default User <132760017+kyo2929@users.noreply.github.com>
2023-08-15 09:03:04 +0000

    [Start]customer-post-comment

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,2 +1,21 @@
 class Public::PostCommentsController < ApplicationController
+  
+  def create
+    @post = Post.find(params[:post_id])
+    comment = current_customer.post_comments.new(post_comment_params)
+    comment.post_id = @post.id
+    comment.save
+    redirect_to post_path(@post.id)
+  end
+
+  def destroy
+    BookComment.find(params[:id]).destroy
+    redirect_to request.referer
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
 end

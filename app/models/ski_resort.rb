85a6ebc86515e8c7d874fe9bd61623afb1664fe6
EC2 Default User <132760017+kyo2929@users.noreply.github.com>
2023-08-28 11:34:59 +0000

    [Update]admin-skiresort

--- app/models/ski_resort.rb
+++ app/models/ski_resort.rb
@@ -3,4 +3,10 @@ class SkiResort < ApplicationRecord
   has_many :posts
 
   has_many_attached :ski_resort_images
+  
+  validates :name,          presence: true, length: { minimum: 2, maximum: 200 }, uniqueness: true
+  validates :introduction,  presence: true, length: { maximum: 100 }
+  validates :address,       presence: true
+  validates :prefecture_id, presence: true
+  
 end
